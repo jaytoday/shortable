dfe3d0eadad163353cb8f5d654b59c2f9c6ca446
james <james@james-desktop.(none)>
2009-04-26 16:17:34 -0700

    fixed small typo in utils

--- utils/utils.py
+++ utils/utils.py
@@ -93,9 +93,7 @@ def memoize(key, time=1000000):
         def wrapper(*args, **kwargs):
             from google.appengine.api import memcache
             data = memcache.get(key)
-            print ""
-            print data
-          #  if Debug(): return fxn(*args, **kwargs) # not active in dev mode - could this be cause of production expires problem?
+            if Debug(): return fxn(*args, **kwargs) # not active in dev mode - could this be cause of production expires problem?
             if data is not None:
                 return data
             data = fxn(*args, **kwargs)
